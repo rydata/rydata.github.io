9685c223f85079b67d07220064c3ec50e225ce79
rydata <lotillaryan@gmail.com>
2023-11-23 15:39:23 +0800

    5th test commit

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-1700724854';
+const cacheName = 'chirpy-1700725060';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
