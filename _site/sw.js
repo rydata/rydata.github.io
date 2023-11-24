91ae6b5f9005fecd5ad911efea36a05edeb3eb05
rydata <lotillaryan@gmail.com>
2023-11-24 15:42:49 +0800

    Favicon update

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-1700744861';
+const cacheName = 'chirpy-1700810009';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
