27d6ce9de943656fefdc662a877486a0ea747a18
rydata <lotillaryan@gmail.com>
2023-11-25 02:37:11 +0800

    hr analysis updated

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-1700820764';
+const cacheName = 'chirpy-1700850896';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
