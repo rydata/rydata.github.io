815492e55121f5e7cfaa3fa55d77d2a10fef2d8c
rydata <lotillaryan@gmail.com>
2023-11-29 17:53:48 +0800

    excel project update

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-1700850896';
+const cacheName = 'chirpy-1701251606';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
