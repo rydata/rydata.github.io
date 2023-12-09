89ebef6bba3d26217af688c24de6d91b4d32c374
rydata <lotillaryan@gmail.com>
2023-12-09 20:01:35 +0800

    sidebar links update

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-1702122655';
+const cacheName = 'chirpy-1702123275';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
